3995127714684ba7686433a8e7264bc7801daf83
mohamadwehbe <wehbe148@gmail.com>
2021-11-01 19:18:50 +0200

    starting

--- javascript.js
+++ javascript.js
@@ -1,9 +1,23 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-   
-  readline.question('Who are you?', name => {
-    console.log(`Hey there ${name}!`);
-    readline.close();
-  });
+const prompt = require('prompt-sync')({sigint: true});
+
+let guess = false;
+ 
+while (!guess) {
+
+  let option = prompt('choose an option: ');
+
+  if (option === "q" || option === "Q" ) {
+    console.log('exit program!');
+    guess = true;
+  }
+  else if( option === "a" || option === "A" )
+    console.log(` option is : ${option} `);
+  else if( option === "b" || option === "B" )
+    console.log(` option is : ${option} `);
+  else if( option === "c" || option === "C" )
+    console.log(` option is : ${option} `);
+  else if( option === "d" || option === "D" )
+    console.log(` option is : ${option} `);
+  else
+    console.log(` ${option} is not an option, choose again! `);
+}

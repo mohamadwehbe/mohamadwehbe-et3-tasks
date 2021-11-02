57188a3f4fa3a741804aa550ea3caf34403b94c1
mohamadwehbe <wehbe148@gmail.com>
2021-11-02 20:31:42 +0200

    costs above 280$

---
+++ above280.js
@@ -0,0 +1,23 @@
+var a = new Array(52,208,31,66,110,5,88,300);
+console.log('The costs above 280$ are : ')
+for(i =0 ; i<a.length;i++) {
+    large = 0;
+    medium=0;
+    small=0;
+    bags = a[i] ;
+    while (bags >= 20) {
+        large++;
+        bags = bags - 20;
+    }
+    while(bags >=10) {
+        medium++;
+        bags = bags - 10;
+    }
+    while(bags > 0) {
+    small++;
+    bags = bags - 5;
+    }
+    cost = large*1.80+medium*1.00+small*0.60+(a[i])*5.50;
+    if(cost > 280)
+        console.log(cost);
+}

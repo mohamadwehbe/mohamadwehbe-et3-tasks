21716168473a0ee98c6ab60454d17c3b7674ccb6
mohamadwehbe <wehbe148@gmail.com>
2021-11-08 18:56:06 +0200

    Order Options

--- options.js
+++ options.js
@@ -23,7 +23,7 @@ while (!guess) {
     let order = prompt('Delete an order: ');
     b = orders.filter(e => e === order);
     b.forEach(f => orders.splice(orders.findIndex(e => e === f),1));
-    console.log(orders);
+    console.log({orders});
   }
 //Find order
   else if( option === "c" || option === "C" ) {
@@ -31,17 +31,19 @@ while (!guess) {
   bool = _.some(orders, order=>order === find);
   if (bool) {
     order = _.filter(orders, order=>order === find);
-    console.log(order);
+    console.log({order});
   }
+  else console.log(' Not Found! ')
   }
 //Checkout(calculate and print all orders)
   else if( option === "d" || option === "D" ) {
     console.log(` Checkout: `);
     orders.map(order=>{
-      large = 0;
+        //count large,medium,and small
+        large = 0;
         medium=0;
         small=0;
-        console.log(`Number of Bags: ${order} bags of coffee!`);
+        console.log(`{ numberOfBags: ${order} }`);
         bags = order ;
         while (bags >= 20) {
             large++;
@@ -55,15 +57,19 @@ while (!guess) {
         small++;
         bags = bags - 5;
         }
-        console.log(`The total price of the coffee ordered is : ${order*5.50}$`)
-        console.log(`Number of Large Boxes is ${large} , the price of large boxes used is ${large*1.80} `);
-        console.log(`Number of Medium Boxes : ${medium} the price of medium boxes used is ${medium*1.00} `);
-        console.log(`Number of Small Boxes : ${small} the price of small boxes used is ${small*0.60} `);
+        priceOfCoffee = order*5.50 ;
+        console.log({priceOfCoffee});
+        console.log(`{ Number of Large Boxes : ${large} , Price of large boxes used : ${large*1.80} }`);
+        console.log(`{ Number of Medium Boxes : ${medium} , Price of medium boxes used : ${medium*1.00} }`);
+        console.log(`{ Number of Small Boxes : ${small} , Price of small boxes used : ${small*0.60} }`);
         if(bags === 0) console.log('The small boxes are full');
         else console.log(`The last small box has ${5+bags} bags of coffee`);
-        console.log(`The total Cost is : ${large*1.80+medium*1.00+small*0.60+(order)*5.50}$`);
+        totalCost = large*1.80+medium*1.00+small*0.60+(order)*5.50 ;
+        console.log({totalCost});
     });
   }
-  else
+  else {
     console.log(` ${option} is not an option, choose again! `);
+    console.log(' The options are : A for Add  , B for Delete , C for Find , D for Checkout and Q for Exit! ');
+  }
 }

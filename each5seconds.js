9403e41c2de24bee402f261cac32ec7ad1db129d
mohamadwehbe <wehbe148@gmail.com>
2021-11-08 18:47:31 +0200

    Calculate some numbers each 5 seconds

--- each5seconds.js
+++ each5seconds.js
@@ -4,10 +4,11 @@ setInterval(function(){
 
     for(i =0 ; i<a.length;i++)
     {
+        //count large,medium,and small
         large = 0;
         medium=0;
         small=0;
-        console.log(`Number of Bags: ${a[i]} bags of coffee!`);
+        console.log(`{ numberOfBags: ${a[i]} }`);
         bags = a[i] ;
         while (bags >= 20) {
             large++;
@@ -21,12 +22,14 @@ setInterval(function(){
         small++;
         bags = bags - 5;
         }
-        console.log(`The total price of the coffee ordered is : ${a[i]*5.50}$`)
-        console.log(`Number of Large Boxes is ${large} , the price of large boxes used is ${large*1.80} `);
-        console.log(`Number of Medium Boxes : ${medium} the price of medium boxes used is ${medium*1.00} `);
-        console.log(`Number of Small Boxes : ${small} the price of small boxes used is ${small*0.60} `);
+        priceOfCoffee = a[i]*5.50 ;
+        console.log({priceOfCoffee});
+        console.log(`{ Number of Large Boxes : ${large} , Price of large boxes used : ${large*1.80} }`);
+        console.log(`{ Number of Medium Boxes : ${medium} , Price of medium boxes used : ${medium*1.00} }`);
+        console.log(`{ Number of Small Boxes : ${small} , Price of small boxes used : ${small*0.60} }`);
         if(bags === 0) console.log('The small boxes are full');
         else console.log(`The last small box has ${5+bags} bags of coffee`);
-        console.log(`The total Cost is : ${large*1.80+medium*1.00+small*0.60+(a[i])*5.50}$`);
+        totalCost = large*1.80+medium*1.00+small*0.60+(a[i])*5.50 ;
+        console.log({totalCost});
     }
 }, 5000);//Runs each 5 seconds

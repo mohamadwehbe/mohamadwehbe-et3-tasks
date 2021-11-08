c5fe41c662c16db8bbf0477350abb7e5d3722a01
mohamadwehbe <wehbe148@gmail.com>
2021-11-08 18:36:30 +0200

    Calculate the cost of coffee,the cost and number of boxes used, and the total cost

--- compute.js
+++ compute.js
@@ -3,31 +3,33 @@ const readline = require('readline').createInterface({
     output: process.stdout
   });
    
-  readline.question('Place Order : ', bag => {
+  readline.question('Place Order : ', bags => {
       //count large,medium,and small
         large = 0;
         medium=0;
         small=0;
-        console.log(`Number of Bags: ${bag} bags of coffee!`);
-        bags = bag ;
-        while (bags >= 20) {
+        console.log({bags});
+        bag = bags ;
+        while (bag >= 20) {
             large++;
-            bags = bags - 20;
+            bag = bag - 20;
         }
-        while(bags >=10) {
+        while(bag >=10) {
             medium++;
-            bags = bags - 10;
+            bag = bag - 10;
         }
-        while(bags > 0) {
+        while(bag > 0) {
         small++;
-        bags = bags - 5;
+        bag = bag - 5;
         }
-        console.log(`The total price of the coffee ordered is : ${bag*5.50}$`)
-        console.log(`Number of Large Boxes is ${large} , the price of large boxes used is ${large*1.80} `);
-        console.log(`Number of Medium Boxes : ${medium} the price of medium boxes used is ${medium*1.00} `);
-        console.log(`Number of Small Boxes : ${small} the price of small boxes used is ${small*0.60} `);
-        if(bags === 0) console.log('The small boxes are full');
-        else console.log(`The last small box has ${5+bags} bags of coffee`);
-        console.log(`The total Cost is : ${large*1.80+medium*1.00+small*0.60+(bag)*5.50}$`);
+        priceOfCoffee = bags*5.50 ;
+        console.log({priceOfCoffee});
+        console.log(`{ Number of Large Boxes : ${large} , Price of large boxes used : ${large*1.80} }`);
+        console.log(`{ Number of Medium Boxes : ${medium} , Price of medium boxes used : ${medium*1.00} }`);
+        console.log(`{ Number of Small Boxes : ${small} , Price of small boxes used : ${small*0.60} }`);
+        if(bag === 0) console.log('{The small boxes are full}');
+        else console.log(`{ The last small box has ${5+bag} bags of coffee }`);
+        totalCost = large*1.80+medium*1.00+small*0.60+(bags)*5.50 ;
+        console.log({totalCost});
     readline.close();
   });

a1c7890764a8f6e1bfbfbd27a7f523d628988286
mohamadwehbe <wehbe148@gmail.com>
2021-11-09 14:19:21 +0200

    calculate the cost each 5 seconds

--- each5seconds.js
+++ each5seconds.js
@@ -1,35 +1,34 @@
-setInterval(function(){ 
+var a = new Array(52,208,31,66,110,5,88,300);
 
-    var a = new Array(52,208,31,66,110,5,88,300);
+setInterval(()=>{ 
 
-    for(i =0 ; i<a.length;i++)
-    {
-        //count large,medium,and small
-        large = 0;
-        medium=0;
-        small=0;
-        console.log(`{ numberOfBags: ${a[i]} }`);
-        bags = a[i] ;
-        while (bags >= 20) {
-            large++;
-            bags = bags - 20;
+    a.map(bags=>{
+        large = medium = small = contains =0;
+        tempBags = bags;
+        if(tempBags >= 20) {
+            large = (tempBags - tempBags % 20)/20;
+            tempBags = tempBags%20;
         }
-        while(bags >=10) {
-            medium++;
-            bags = bags - 10;
+        if(tempBags >= 10) {
+            medium = (tempBags - tempBags % 10)/10;
+            tempBags = tempBags%10;
         }
-        while(bags > 0) {
-        small++;
-        bags = bags - 5;
+        if(tempBags >= 5) {
+            small = (tempBags - tempBags % 5)/5;
+            tempBags = tempBags%5;
         }
-        priceOfCoffee = a[i]*5.50 ;
+        if(tempBags > 0) {
+            small ++;
+            contains = tempBags ;
+        }
+        priceOfCoffee = bags*5.50 ;
         console.log({priceOfCoffee});
         console.log(`{ Number of Large Boxes : ${large} , Price of large boxes used : ${large*1.80} }`);
         console.log(`{ Number of Medium Boxes : ${medium} , Price of medium boxes used : ${medium*1.00} }`);
         console.log(`{ Number of Small Boxes : ${small} , Price of small boxes used : ${small*0.60} }`);
-        if(bags === 0) console.log('The small boxes are full');
-        else console.log(`The last small box has ${5+bags} bags of coffee`);
-        totalCost = large*1.80+medium*1.00+small*0.60+(a[i])*5.50 ;
+        if(contains > 0) console.log(`{ The last small box has ${contains} bags of coffee }`);
+        totalCost = large*1.80+medium*1.00+small*0.60+(bags)*5.50 ;
         console.log({totalCost});
-    }
+    })
+
 }, 5000);//Runs each 5 seconds

ac9505d6f6c6e52699fb15e01da30184e7d104bf
mohamadwehbe <wehbe148@gmail.com>
2021-11-09 14:22:33 +0200

    Order Options

--- options.js
+++ options.js
@@ -38,34 +38,34 @@ while (!guess) {
 //Checkout(calculate and print all orders)
   else if( option === "d" || option === "D" ) {
     console.log(` Checkout: `);
-    orders.map(order=>{
-        //count large,medium,and small
-        large = 0;
-        medium=0;
-        small=0;
-        console.log(`{ numberOfBags: ${order} }`);
-        bags = order ;
-        while (bags >= 20) {
-            large++;
-            bags = bags - 20;
-        }
-        while(bags >=10) {
-            medium++;
-            bags = bags - 10;
-        }
-        while(bags > 0) {
-        small++;
-        bags = bags - 5;
-        }
-        priceOfCoffee = order*5.50 ;
-        console.log({priceOfCoffee});
-        console.log(`{ Number of Large Boxes : ${large} , Price of large boxes used : ${large*1.80} }`);
-        console.log(`{ Number of Medium Boxes : ${medium} , Price of medium boxes used : ${medium*1.00} }`);
-        console.log(`{ Number of Small Boxes : ${small} , Price of small boxes used : ${small*0.60} }`);
-        if(bags === 0) console.log('The small boxes are full');
-        else console.log(`The last small box has ${5+bags} bags of coffee`);
-        totalCost = large*1.80+medium*1.00+small*0.60+(order)*5.50 ;
-        console.log({totalCost});
+    orders.map(bags=>{
+      console.log({bags});
+      large = medium = small = contains =0;
+      tempBags = bags;
+      if(tempBags >= 20) {
+          large = (tempBags - tempBags % 20)/20;
+          tempBags = tempBags%20;
+      }
+      if(tempBags >= 10) {
+          medium = (tempBags - tempBags % 10)/10;
+          tempBags = tempBags%10;
+      }
+      if(tempBags >= 5) {
+          small = (tempBags - tempBags % 5)/5;
+          tempBags = tempBags%5;
+      }
+      if(tempBags > 0) {
+          small ++;
+          contains = tempBags ;
+      }
+      priceOfCoffee = bags*5.50 ;
+      console.log({priceOfCoffee});
+      console.log(`{ Number of Large Boxes : ${large} , Price of large boxes used : ${large*1.80} }`);
+      console.log(`{ Number of Medium Boxes : ${medium} , Price of medium boxes used : ${medium*1.00} }`);
+      console.log(`{ Number of Small Boxes : ${small} , Price of small boxes used : ${small*0.60} }`);
+      if(contains > 0) console.log(`{ The last small box has ${contains} bags of coffee }`);
+      totalCost = large*1.80+medium*1.00+small*0.60+(bags)*5.50 ;
+      console.log({totalCost});
     });
   }
   else {

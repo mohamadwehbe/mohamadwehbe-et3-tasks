c5d4d4379c9b5485aa2b9f6eba60afc33d608e8a
mohamadwehbe <wehbe148@gmail.com>
2021-11-09 14:13:16 +0200

    costs above 280$

--- above280.js
+++ above280.js
@@ -1,23 +1,27 @@
 var a = new Array(52,208,31,66,110,5,88,300);
 console.log('The costs above 280$ are : ')
-for(i =0 ; i<a.length;i++) {
-    large = 0;
-    medium=0;
-    small=0;
-    bags = a[i] ;
-    while (bags >= 20) {
-        large++;
-        bags = bags - 20;
+a.map(bags=>{
+    large = medium = small = contains =0;
+    tempBags = bags;
+    if(tempBags >= 20) {
+        large = (tempBags - tempBags % 20)/20;
+        tempBags = tempBags%20;
     }
-    while(bags >=10) {
-        medium++;
-        bags = bags - 10;
+    if(tempBags >= 10) {
+        medium = (tempBags - tempBags % 10)/10;
+        tempBags = tempBags%10;
     }
-    while(bags > 0) {
-    small++;
-    bags = bags - 5;
+    if(tempBags >= 5) {
+        small = (tempBags - tempBags % 5)/5;
+        tempBags = tempBags%5;
     }
-    cost = large*1.80+medium*1.00+small*0.60+(a[i])*5.50;
-    if(cost > 280)
+    if(tempBags > 0) {
+        small ++;
+        contains = tempBags ;
+    }
+    cost = large*1.80+medium*1.00+small*0.60+(bags)*5.50;
+    if(cost > 280) {
+        console.log({bags})
         console.log({cost});
-}
+    }
+})

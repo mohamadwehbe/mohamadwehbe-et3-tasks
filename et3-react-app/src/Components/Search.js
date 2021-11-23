fb86de67910e8ef55b0091ff2291170e559ef5fe
mohamadwehbe <wehbe148@gmail.com>
2021-11-23 19:04:39 +0200

    Updated

--- et3-react-app/src/Components/Search.js
+++ et3-react-app/src/Components/Search.js
@@ -3,6 +3,7 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
 
+
 function sleep(delay = 0) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
